2b187410432e6ad9eb11c76a16f1617d94a68034
sirena backham <sirena-backham@uiowa.edu>
2023-04-15 23:04:19 -0500

    fix

--- oral_exam2/MorseCode/src/MorseCodeDriver.java
+++ oral_exam2/MorseCode/src/MorseCodeDriver.java
@@ -1,5 +1,22 @@
-
+import java.util.Scanner;
 
 public class MorseCodeDriver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter '1' if you are encoding English to Morse Code and enter '2' if you are decoding Morse to English" );
+        int encodeOrDecode = scanner.nextInt();
+
+        System.out.println("You may enter the message you will be encoding or decoding. Do NOT add extra spaces in the beginning or end" );
+        String message = scanner.next();
+
+        if(encodeOrDecode == 1){
+            System.out.println("Here is your message encoded in Morse Code: ");
+            System.out.println(MorseCodeDecoder.EnglishToMorse(message));
+        }
+        if(encodeOrDecode == 2){
+            System.out.println("Here is your message decoded in English: ");
+            System.out.println(MorseCodeDecoder.MorseToEnglish(message));
+        }
+    }
 }
